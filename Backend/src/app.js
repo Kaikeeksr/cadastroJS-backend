3d7c20e1e5b95f1c7a0ea926112f4242c59cbdad
Kaike <kaikeksr@outlook.com>
2023-09-05 11:19:23 -0300

    CORS config

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const router = require("./router")
+const cors = require("cors")
 
 const app = express()
 
+app.use(cors())
 app.use(express.json()) //configurando a API para trabalhar com dados JSON
 app.use(router)
 
